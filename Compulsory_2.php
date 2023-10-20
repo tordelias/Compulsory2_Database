cf0196200f2cad46304f2b6443d7c19015d8fe2c
Tord-Elias <tordeliashoumb46@gmail.com>
2023-10-20 19:24:38 +0200

    you can now see results

--- Compulsory_2.php
+++ Compulsory_2.php
@@ -21,5 +21,28 @@
             exit();
         } 
         else {
-            printf("alt ok!");
+   //         printf("alt ok!");
         }
+        
+        $setning  = "SELECT * FROM playerdata";
+   //  echo $setning;
+     $resultat = mysqli_query($mysql, $setning);
+     $rad = mysqli_fetch_assoc($resultat);
+     if($rad == 0)
+         echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
+     echo "<p>";
+             echo '<table border = "1">';
+        
+        echo '<caption><h3>Player data</h3></caption>';    
+        echo '<tr><th>PlayerID</th><th>PlayerName</th><th>PlayerRank</th><th>Banned</th><th>TempBans</th></tr>';
+            do
+            {
+                echo '<tr>
+                <td>'.$rad['PlayerID'].'</td>
+                <td>'.$rad['PlayerName'].'</td>
+                <td>'.$rad['PlayerRank'].'</td>
+                 <td>'.$rad['Banned'].'</td>
+                 <td>'.$rad['TempBans'].'</td>
+                </tr>';
+            } while ($rad = mysqli_fetch_assoc($resultat));
+            echo'</table>';
