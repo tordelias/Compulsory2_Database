99a9cde3c2e14fab5efc8e2b103e45dfe9cbf5e5
Hans <111863567+HansPluss@users.noreply.github.com>
2023-10-20 18:55:43 +0200

    Updated text

--- databasesqlscript.sql
+++ databasesqlscript.sql
@@ -1,21 +1,21 @@
 CREATE TABLE `playerdata` (
-  `Player ID` int NOT NULL,
-  `Player Name` varchar(245) NOT NULL,
+  `PlayerID` int NOT NULL,
+  `PlayerName` varchar(245) NOT NULL,
   `Rank` varchar(45) DEFAULT NULL,
   `Banned` tinyint(1) NOT NULL,
   `Temp Bans` int DEFAULT NULL,
   PRIMARY KEY (`Player ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `transaction` (
-  `Player ID` int NOT NULL,
-  `Item Nr` int DEFAULT NULL,
+  `PlayerID` int NOT NULL,
+  `ItemNr` int DEFAULT NULL,
   `Price` int DEFAULT NULL,
-  `Order Nr` int NOT NULL,
+  `OrderNr` int NOT NULL,
   `Date` date NOT NULL,
   PRIMARY KEY (`Player ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 CREATE TABLE `items` (
-  `Item Nr` int NOT NULL,
+  `ItemNr` int NOT NULL,
   `Price` int DEFAULT NULL,
   `Item` varchar(45) DEFAULT NULL,
   `Sold` int DEFAULT NULL,
