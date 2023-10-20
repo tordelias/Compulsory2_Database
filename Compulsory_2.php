04ab204d6dd18df71e0a20061c3563d9f543ce18
Tord-Elias <tordeliashoumb46@gmail.com>
2023-10-20 22:27:02 +0200

    added the lists of conntent for the tables

--- Compulsory_2.php
+++ Compulsory_2.php
@@ -1,71 +1,107 @@
-    <?php
-
-
-            echo "<h1>Compulsory 2 Website</h1>";
-            print "<p></p>";
-            echo "<p></p>";
-            echo "<p></p>";
-            echo "<p>Paragraf</p>";
+<?php
 
 
 
-        $Host = 'localhost';
-        $User = 'root';
-        $Password = '';
-        $Database = 'compulsory2';
-        
-        $mysql = mysqli_connect($Host, $User, $Password, $Database);
-        
-        if(mysqli_connect_errno()){
-            printf("ingen forbindelse : %s", mysqli_connect_error());
-            exit();
-        } 
-        else {
-   //         printf("alt ok!");
-        }
-        
-        $setning  = "SELECT * FROM playerdata";
-   //  echo $setning;
-     $resultat = mysqli_query($mysql, $setning);
-     $rad = mysqli_fetch_assoc($resultat);
-     if($rad == 0)
-         echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
-     echo "<p>";
-             echo '<table border = "1">';
-        
-        echo '<caption><h3>Player data</h3></caption>';    
-        echo '<tr><th>PlayerID</th><th>PlayerName</th><th>PlayerRank</th><th>Banned</th><th>TempBans</th></tr>';
-            do
-            {
-                echo '<tr>
-                <td>'.$rad['PlayerID'].'</td>
-                <td>'.$rad['PlayerName'].'</td>
-                <td>'.$rad['PlayerRank'].'</td>
-                 <td>'.$rad['Banned'].'</td>
-                 <td>'.$rad['TempBans'].'</td>
-                </tr>';
-            } while ($rad = mysqli_fetch_assoc($resultat));
-            echo'</table>';
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<style>";
+echo "body { background-color: #f2f2f2; }"; // Set the background color to grayish
+echo ".cool-table { background-color: #fff; border: 2px solid #ccc; border-radius: 5px; padding: 10px; margin-right: 10px; }";
+echo "table { width: 100%; border-collapse: collapse; margin-bottom: 20px; }"; // Added margin-bottom
+echo "table, th, td { border: 2px solid #ddd; }"; // Increased border thickness
+echo "th, td { padding: 10px; text-align: left; }";
+echo "th { background-color: #333; color: #fff; }";
+echo "</style>";
+echo "</head>";
+echo "<body>";
 
-      $setning2  = "SELECT * FROM items";
-   //  echo $setning;
-     $resultat2 = mysqli_query($mysql, $setning2);
-     $rad2 = mysqli_fetch_assoc($resultat2);
-     if($rad2 == 0)
-         echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
-     echo "<p>";
-             echo '<table border = "1">';
-        
-        echo '<caption><h3> Items </h3></caption>';    
-        echo '<tr><th>ItemNr</th><th>Price</th><th>Item</th><th>Sold</th><th>Profit</th></tr>';
-            do
-            {
-                echo '<tr>
-                <td>'.$rad2['ItemNr'].'</td>
-                <td>'.$rad2['Price'].'</td>
-                <td>'.$rad2['Item'].'</td>
-                 <td>'.$rad2['Sold'].'</td>
-                 <td>'.$rad2['Profit'].'</td>
-                </tr>';
-            } while ($rad2 = mysqli_fetch_assoc($resultat2));
-            echo'</table>';
+$Host = 'localhost';
+$User = 'root';
+$Password = '';
+$Database = 'compulsory2';
+
+$mysql = mysqli_connect($Host, $User, $Password, $Database);
+
+if (mysqli_connect_errno()) {
+    printf("ingen forbindelse : %s", mysqli_connect_error());
+    exit();
+} else {
+    // printf("alt ok!");
+}
+
+echo '<div style="display: flex;">';
+
+// First table
+$setning  = "SELECT * FROM playerdata";
+//  echo $setning;
+$resultat = mysqli_query($mysql, $setning);
+$rad = mysqli_fetch_assoc($resultat);
+if ($rad == 0)
+    echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
+echo "<p>";
+echo '<table border="1" class="cool-table">'; // Added class attribute
+echo '<caption><h3>Player data</h3></caption>';
+echo '<tr><th>PlayerID</th><th>PlayerName</th><th>PlayerRank</th><th>Banned</th><th>TempBans</th></tr>';
+do {
+    echo '<tr>
+        <td>' . $rad['PlayerID'] . '</td>
+        <td>' . $rad['PlayerName'] . '</td>
+        <td>' . $rad['PlayerRank'] . '</td>
+        <td>' . $rad['Banned'] . '</td>
+        <td>' . $rad['TempBans'] . '</td>
+        </tr>';
+} while ($rad = mysqli_fetch_assoc($resultat));
+echo '</table>';
+
+// Second table
+$setning  = "SELECT * FROM items";
+//  echo $setning;
+$resultat = mysqli_query($mysql, $setning);
+$rad = mysqli_fetch_assoc($resultat);
+if ($rad == 0)
+    echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
+echo "<p>";
+echo '<table border="1" class="cool-table">'; // Added class attribute
+
+echo '<caption><h3> Items </h3></caption>';
+echo '<tr><th>ItemNr</th><th>Price</th><th>Item</th><th>Sold</th><th>Profit</th></tr>';
+do {
+    echo '<tr>
+        <td>' . $rad['ItemNr'] . '</td>
+        <td>' . $rad['Price'] . '</td>
+        <td>' . $rad['Item'] . '</td>
+        <td>' . $rad['Sold'] . '</td>
+        <td>' . $rad['Profit'] . '</td>
+        </tr>';
+} while ($rad = mysqli_fetch_assoc($resultat));
+echo '</table>';
+
+// Third Table
+$setning  = "select * from transaction";
+//  echo $setning;
+$resultat = mysqli_query($mysql, $setning);
+$rad = mysqli_fetch_assoc($resultat);
+if ($rad == 0)
+    echo '<meta http-equiv="refresh" content="2 ; URL=index.php">';
+echo "<p>";
+echo '<table border="1" class="cool-table">'; // Added class attribute
+
+echo '<caption><h3> Transactions </h3></caption>';
+echo '<tr><th>PlayerID</th><th>ItemNr</th><th>Price</th><th>OrderNr</th><th>Date</th></tr>';
+do {
+    echo '<tr>
+        <td>' . $rad['PlayerID'] . '</td>
+        <td>' . $rad['ItemNr'] . '</td>
+        <td>' . $rad['Price'] . '</td>
+        <td>' . $rad['OrderNr'] . '</td>
+        <td>' . $rad['Date'] . '</td>
+        </tr>';
+} while ($rad = mysqli_fetch_assoc($resultat));
+echo '</table>';
+
+echo '</div>';
+
+echo "</body>";
+echo "</html>";
+?>
---
+++ playerdata.sql
@@ -0,0 +1,55 @@
+/*
+-- Query: SELECT * FROM playerdata
+LIMIT 0, 1000
+
+-- Date: 2023-10-20 22:21
+*/
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (1,'RuneMaster','bronze',0,3);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (2,'SorcererSupreme','diamond',0,11);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (3,'ArcaneArcher','silver',0,5);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (4,'TimeTraveler','gold',0,5);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (5,'DivineDancer','gold',0,9);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (6,'EternalScribe','diamond',0,18);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (7,'MysticMage','silver',0,7);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (8,'Drixe','Dirt',0,1000);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (9,'BlazingBard','bronze',0,12);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (10,'CrystalCaster','diamond',0,4);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (11,'SerpentSorcerer','gold',0,5);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (12,'FrostFairy','diamond',0,13);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (13,'SoulReaver','gold',0,4);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (14,'SeaSorceress','gold',0,9);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (15,'ShadowWitch','platinum',0,7);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (16,'MysticMusketeer','bronze',0,18);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (17,'AstralWanderer','silver',0,10);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (18,'WarriorPriest','platinum',0,3);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (19,'ShadowStalker','gold',0,14);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (20,'LoneWolf','silver',0,9);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (21,'MysticMarauder','platinum',0,8);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (22,'GlimmerGirl','silver',0,5);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (23,'RogueRanger','gold',0,6);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (24,'EarthShaman','diamond',0,2);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (25,'RogueRider','silver',0,11);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (26,'StarSeeker','bronze',0,13);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (27,'Babab boy','gold',0,10);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (28,'SwordSage','silver',0,9);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (29,'FireDragon','diamond',0,15);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (30,'IronKnight','diamond',0,12);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (31,'EternalExplorer','platinum',0,10);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (32,'NatureGuardian','bronze',0,22);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (33,'ThunderStriker','silver',0,8);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (34,'ForestGuardian','silver',0,2);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (35,'FlameLord','gold',0,11);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (36,'StormRider','gold',0,8);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (37,'NatureNymph','diamond',0,7);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (38,'DragonSlayer','platinum',0,12);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (39,'ShadowNinja','platinum',0,20);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (40,'CrimsonCleric','gold',0,4);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (41,'FrostQueen','bronze',0,10);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (42,'SwiftAssassin','platinum',0,15);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (43,'Kiffy','wood',0,100);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (44,'BardicBard','silver',0,14);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (45,'TimeTwister','diamond',0,15);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (46,'IceQueen','bronze',0,2);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (47,'CrystalGazer','silver',0,6);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (48,'WanderingWizard','diamond',0,6);
+INSERT INTO `` (`PlayerID`,`PlayerName`,`PlayerRank`,`Banned`,`TempBans`) VALUES (49,'MoltenMagician','bronze',0,21);
