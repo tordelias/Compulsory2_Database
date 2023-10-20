38909f5880b943bb77f2e5ffec5e1a520e62dac9
Tord-Elias <tordeliashoumb46@gmail.com>
2023-10-20 18:06:17 +0200

    fixed some small stuff

--- Compulsory_2.php
+++ Compulsory_2.php
@@ -1,13 +1,18 @@
     <?php
+
+    echo "Compulsory 2 website";
+
         $Host = 'localhost';
         $User = 'root';
         $Password = '';
-        $Database = 'Compulsory2';
+        $Database = 'compulsory2';
         
         $mysql = mysqli_connect($Host, $User, $Password, $Database);
         
         if(mysqli_connect_errno()){
             printf("ingen forbindelse : %s", mysqli_connect_error());
             exit();
-        } else {
+        } 
+        else {
 //            printf("alt ok!")
+        }
