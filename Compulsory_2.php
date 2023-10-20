1c10cc30e2bec241b2e1163934c914d0200acce9
Tord-Elias <tordeliashoumb46@gmail.com>
2023-10-20 17:36:27 +0200

    added a connection to a database

--- Compulsory_2.php
+++ Compulsory_2.php
@@ -1,1 +1,13 @@
-html
+    <?php
+        $Host = 'localhost';
+        $User = 'root';
+        $Password = '';
+        $Database = 'Compulsory2';
+        
+        $mysql = mysqli_connect($Host, $User, $Password, $Database);
+        
+        if(mysqli_connect_errno()){
+            printf("ingen forbindelse : %s", mysqli_connect_error());
+            exit();
+        } else {
+//            printf("alt ok!")
